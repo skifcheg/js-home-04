08dd953420c0a14864611686434af1e76d446ba2
Sasha <skif@ki-technology.ru>
2016-07-14 01:05:02 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
 confirm("Чи бажаєте почати гру?") ? Game() : GameOver();
 
-function Game(){
-    console.log('GO!');
+
+
+var yourMoney = [10, 5, 2];
+var maxNum = 5;
+var countTry = 0;
+var i = 0;
+var count = 3;
+
+
+function Game(countTry){
+  console.log('GO!', countTry);
+  maxNum = maxNum * countTry;
+  var num = parseInt(Math.random()*maxNum, 10);
+  var userTry = prompt("Вгадай число від 0 до " + maxNum);
+
+  do{
+    if(userTry == num){
+      confirm("хоче продовжити гру?") ? Game(countTry*2) : GameWin(yourMoney[i]);
+    }
+    else{
+      GameOver();
+      break;
+    }
+    i++;
+  }while(i < count);
+}
+function GameWin(x){
+  console.info('Дякуємо за гру, ваш виграш становить ' + x + '$');
+}
+function GameOver(){
+  console.info("Сьогодні ви не виграли мільйон, а могли");
 }
 
-function GameOver(){
-    console.info("Сьогодні ви не виграли мільйон, а могли");
-}
-//alert("Сьогодні ви не виграли мільйон, а могли");
 //confirm();
 //
-//console.info('Дякуємо за гру, ваш виграш становить ' + yourMoney);
+
