516b60c4c9c41f2d1575e5b91fb0adc2941a54df
admin <admin@MacBook-Pro-admin.local>
2016-07-14 20:58:14 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
-confirm("Чи бажаєте почати гру?") ? Game() : GameOver();
 
+confirm("Чи бажаєте почати гру?") ? Game(1) : GameOver();
 
+function Game(gameTry){
 
-var yourMoney = [10, 5, 2];
-var maxNum = 5;
-var countTry = 0;
-var i = 0;
-var count = 3;
+    var yourMoney = [10, 5, 2];
+    var countTry = 1;
+    var maxCountTry = 3;
+    var maxNum = 5 * gameTry;
+    var num = parseInt(Math.random()*maxNum, 10);
 
+    console.log('gameTry ' + gameTry);
 
-function Game(countTry){
-  console.log('GO!', countTry);
-  maxNum = maxNum * countTry;
-  var num = parseInt(Math.random()*maxNum, 10);
-  var userTry = prompt("Вгадай число від 0 до " + maxNum);
-
-  do{
-    if(userTry == num){
-      confirm("хоче продовжити гру?") ? Game(countTry*2) : GameWin(yourMoney[i]);
+    for (var i = 0; i < yourMoney.length; i++) {
+        yourMoney[i] = yourMoney[i] * 3 * gameTry;
     }
-    else{
-      GameOver();
-      break;
+
+    maxNum = maxNum * gameTry;
+    console.log('countTry ' + countTry, 'maxNum ' + maxNum, 'gameTry ' + gameTry, 'num ' + num);
+
+    for(var countTry = 0; countTry < maxCountTry; countTry++){
+
+        console.log('yourMoney ' + yourMoney[countTry] + '$', 'countTry ' + countTry);
+
+        var userNum = prompt("Вгадай число від 0 до " + maxNum);
+        console.log('userNum ' + userNum);
+
+        if(userNum == num){
+            console.log('win', 'gameTry' + gameTry, 'countTry' + countTry);
+          //confirm("хоче продовжити гру?") ? Game(countTry*2) : GameWin(yourMoney[i]);
+        }
+        else{
+            console.log('fail', 'countTry ' + countTry);
+            console.log('maxCountTry ' + maxCountTry);
+
+            //GameOver();
+        }
     }
-    i++;
-  }while(i < count);
 }
 function GameWin(x){
   console.info('Дякуємо за гру, ваш виграш становить ' + x + '$');
